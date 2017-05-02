6fa53790ec9f305d96351e10ec14c8c68d7d4f7e
matijastampfel <matija.stampfel@gmail.com>
2017-05-02 15:10:00 +0200

    v 0.8

---
+++ src/App.js
@@ -0,0 +1,51 @@
+/* eslint-disable */
+// eslint-disable-next-line
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import './App.css';
+const API_KEY = 'AIzaSyCQMSAf-4GdhWVd8YrSC43s-OskR0GzT98';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+         };
+
+      this.videoSearch("Miike Snow - Genghis Khan");  
+    }
+
+videoSearch(term){
+
+YTSearch({key: API_KEY, term:term}, (videos)=>{
+            this.setState({
+                videos: videos,
+                selectedVideo:videos[0]
+        });
+          
+});
+
+}
+  render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+
+    return (
+      <div className="App">
+   <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+        onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos}/>
+    </div>
+    );
+}};
+
+ReactDOM.render(<App />, document.querySelector('.container'))
